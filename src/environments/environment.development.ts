625aaee79fdf4535304bbce745de08ebda3bf910
Eduardo Leon <82557141+AmazingE-dot@users.noreply.github.com>
2024-06-11 21:12:11 -0500

    Pruebas

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  base_url: 'http://localhost:4000/api/v1',
+  // base_url: 'http://localhost:4000/api/v1',
+  base_url: 'https://betidytalentotechback.onrender.com/api/v1'
 };
